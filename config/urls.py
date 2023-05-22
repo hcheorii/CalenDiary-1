2ee17573a34e4d679c54d59879493d76552be214
junh7323 <junh7323@naver.com>
2023-05-22 22:46:33 +0900

    docs: view

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from posts.views import url_view, url_parameter_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('url/', url_view),
+    path('url/<str:username>/', url_parameter_view)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse, JsonResponse
+
+def url_view(request):
+    print('url_view()')
+    data = {'code': '001', 'msg': 'OK'}
+    return HttpResponse('<h1>url_view</h1>')
+    #return JsonResponse(data)
+
+def url_parameter_view(request, username):
+    print('url_parameter_view()')
+    print(f'username: {username}')
+    print(f'request.GET:{request.GET}')
+    return HttpResponse(username)
+
+#@login_required
+def post_create_view(request):
+    if request.method == 'GET':
+        return render(request, 'posts/post_form.html')
+    else:
+        image = request.FILES.get('image')
+        content = request.POST.get('content')
+        print(image)
+        print(content)
+        Post.objects.create(
+            image=image,
+            content=content,
+            author=request.user
+        )
+        return redirect('index')
+    
+def post_form_view(request):
+    if request.method == 'GET':
+        return render(request, 'posts/post_form.html')
+    else:
+        Post.objects.create(
+            image=image,
+            content=content,
+            author=request.user
+        )
+        return redirect('index')
+
+
